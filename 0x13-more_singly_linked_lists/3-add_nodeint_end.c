35555a1e3ce4ffd63be6949d6f97ead4a12ce7ee
samaara001 <asmae.merzougui@gmail.com>
2023-05-01 17:17:52 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,70 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
+
+
+
+
+4-free_listint.c
+
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
